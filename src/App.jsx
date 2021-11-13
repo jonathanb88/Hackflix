c9195f5b74acda77f4db6d0a33ca7b7f08c0a55f
jonathanb88 <jonhybw@gmail.com>
2021-11-13 19:06:42 -0300

    Correcion de nueva version de react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MovieDetails from "./pages/MovieDetails";
 import Home from "./pages/Home";
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Search from "./pages/Search"
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Search from "./pages/Search";
 import NotFound from "./pages/NotFound";
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
-        
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/peliculas/:id" component={MovieDetails}/>
-        <Route path="/about" component={About}/>
-        <Route path="/search" component={Search}/>
-        <Route path="/contact" component={Contact}/>
-        <Route component={NotFound}/>
-      </Switch>
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/peliculas/:id" component={MovieDetails} />
+          <Route path="/about" component={About} />
+          <Route path="/search" component={Search} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Routes>
       </BrowserRouter>
     </>
   );
