5c7a7d13278c9a7172f0ea9a47468706846d6f96
jonathanb88 <jonhybw@gmail.com>
2021-11-14 15:01:25 -0300

    Update npm build

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NotFound() {
   return (
     <div className="text-center d-flex justify-content-center align-self-center">
-      <h2>Error 404 – Page not found</h2>
+      <h2>Error 404 – "Page not found"</h2>
     </div>
   );
 }
