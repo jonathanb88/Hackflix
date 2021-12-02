a3e2deefc49efa9dd7ebec54d1ca5efdd7862306
jonathanb88 <jonhybw@gmail.com>
2021-12-02 18:35:28 -0300

    error

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
+import React from "react";
 import Navbar1 from "../components/Navbar";
 import { Container } from "react-bootstrap";
 
 function Contact() {
-    return (
-        <div className="vh-100">
+  return (
+    <div className="vh-100">
       <Navbar1 />
       <Container>
-        <h3>Contact <span className="text-primary">Hackflix</span> </h3>
+        <h3>
+          Contact <span className="text-primary">Hackflix</span>{" "}
+        </h3>
         <hr />
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
           iure reprehenderit impedit iusto veritatis obcaecati totam eaque
           accusamus! Saepe ipsa, voluptatum eligendi quo ipsam praesentium
-          maiores exercitationem natus repellendus. Repellat.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-          iure reprehenderit impedit iusto veritatis obcaecati totam eaque
-          accusamus! Saepe ipsa, voluptatum eligendi quo ipsam praesentium
-          maiores exercitationem natus repellendus. Repellat.
+          maiores exercitationem natus repellendus. Repellat. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Explicabo iure reprehenderit
+          impedit iusto veritatis obcaecati totam eaque accusamus! Saepe ipsa,
+          voluptatum eligendi quo ipsam praesentium maiores exercitationem natus
+          repellendus. Repellat.
         </p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ullam
           dicta debitis. Excepturi officia vel, architecto maxime esse soluta
           aliquam maiores consequuntur ipsam! Ipsam laboriosam voluptas
-          voluptates optio, ab harum?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ullam
-          dicta debitis. Excepturi officia vel, architecto maxime esse soluta
-          aliquam maiores consequuntur ipsam! Ipsam laboriosam voluptas
-          voluptates optio, ab harum?
+          voluptates optio, ab harum? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Maxime ullam dicta debitis. Excepturi officia vel,
+          architecto maxime esse soluta aliquam maiores consequuntur ipsam!
+          Ipsam laboriosam voluptas voluptates optio, ab harum
         </p>
       </Container>
     </div>
-    )
+  );
 }
 
-export default Contact
+export default Contact;
